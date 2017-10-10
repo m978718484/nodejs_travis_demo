e8c403f6d4a2c86f5aa235c6ef27c019cdf2ff14
m978718484 <m978718484@163.com>
2017-10-10 17:48:14 +0800

    t

--- src/components/Vux.vue
+++ src/components/Vux.vue
@@ -57,7 +57,7 @@ export default {
     },
     onClick (i) {
       console.log(i)
-    }    
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { AlertPlugin, ToastPlugin, Marquee, MarqueeItem} from 'vux'
+import { AlertPlugin, ToastPlugin, Marquee, MarqueeItem } from 'vux'
 
 Vue.use(AlertPlugin)
 Vue.use(ToastPlugin)
