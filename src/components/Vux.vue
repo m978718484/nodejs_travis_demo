c89d6f2bdc3a4b40719ee3d8c0733b8cc5c00598
m978718484 <m978718484@163.com>
2017-10-10 17:22:04 +0800

    t

--- src/components/Vux.vue
+++ src/components/Vux.vue
@@ -2,15 +2,15 @@
   <div>
     <group-title>3 columns</group-title>
     <grid>
-      <grid-item link="/component/cell" :label="$t('Go to Cell')">
+      <grid-item link="/component/cell" label="跳转到主页">
         <img slot="icon" src="../assets/home.png">
       </grid-item>
-      <grid-item :link="{ path: '/component/cell'}" :label="$t('Go to Cell')">
+      <grid-item :link="{ path: '/component/cell'}" label="跳转到主页">
         <img slot="icon" src="../assets/home.png">
       </grid-item>
       <grid-item link="/component/cell" @on-item-click="onItemClick">
         <img slot="icon" src="../assets/home.png">
-        <span slot="label">{{ $t('Go to Cell') }}</span>
+        <span slot="label">跳转到主页</span>
       </grid-item>
     </grid>
   </div>
