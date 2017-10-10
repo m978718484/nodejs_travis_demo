21c7b031fc60afcf954ae9b41c7f74e1a805db85
m978718484 <m978718484@163.com>
2017-10-10 14:17:39 +0800

    t

--- src/main.js
+++ src/main.js
@@ -3,10 +3,80 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { AlertPlugin, ToastPlugin } from 'vux'
+import Vuex from 'vuex'
+import vuexI18n from 'vuex-i18n'
 
-Vue.use(AlertPlugin)
+
+/** i18n **/
+let store = new Vuex.Store({
+  modules: {
+    i18n: vuexI18n.store
+  }
+})
+
+Vue.use(vuexI18n.plugin, store)
+
+const vuxLocales = require('json-loader!yaml-loader!./locales/all.yml')
+const componentsLocales = require('json-loader!yaml-loader!./locales/components.yml')
+
+const finalLocales = {
+  'en': objectAssign(vuxLocales['en'], componentsLocales['en']),
+  'zh-CN': objectAssign(vuxLocales['zh-CN'], componentsLocales['zh-CN'])
+}
+
+for (let i in finalLocales) {
+  Vue.i18n.add(i, finalLocales[i])
+}
+
+import { DatetimePlugin, CloseDialogsPlugin, ConfigPlugin, BusPlugin, LocalePlugin, DevicePlugin, ToastPlugin, AlertPlugin, ConfirmPlugin, LoadingPlugin, WechatPlugin, AjaxPlugin, AppPlugin } from 'vux'
+
+Vue.use(LocalePlugin)
+const nowLocale = Vue.locale.get()
+if (/zh/.test(nowLocale)) {
+  Vue.i18n.set('zh-CN')
+} else {
+  Vue.i18n.set('en')
+}
+
+store.registerModule('vux', {
+  state: {
+    demoScrollTop: 0,
+    isLoading: false,
+    direction: 'forward'
+  },
+  mutations: {
+    updateDemoPosition (state, payload) {
+      state.demoScrollTop = payload.top
+    },
+    updateLoadingStatus (state, payload) {
+      state.isLoading = payload.isLoading
+    },
+    updateDirection (state, payload) {
+      state.direction = payload.direction
+    }
+  },
+  actions: {
+    updateDemoPosition ({commit}, top) {
+      commit({type: 'updateDemoPosition', top: top})
+    }
+  }
+})
+
+// global VUX config
+Vue.use(ConfigPlugin, {
+  $layout: 'VIEW_BOX' // global config for VUX, since v2.5.12
+})
+
+// plugins
+Vue.use(DevicePlugin)
 Vue.use(ToastPlugin)
+Vue.use(AlertPlugin)
+Vue.use(ConfirmPlugin)
+Vue.use(LoadingPlugin)
+Vue.use(WechatPlugin)
+Vue.use(AjaxPlugin)
+Vue.use(BusPlugin)
+Vue.use(DatetimePlugin)
 
 Vue.config.productionTip = false
 
