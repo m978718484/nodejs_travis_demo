9e0788875b72ca9dc3136e0860cc6747bb263b8c
m978718484 <m978718484@163.com>
2017-10-10 10:35:46 +0800

    use vux

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { AlertPlugin, ToastPlugin } from 'vux'
+
+Vue.use(AlertPlugin)
+Vue.use(ToastPlugin)
 
 Vue.config.productionTip = false
 
