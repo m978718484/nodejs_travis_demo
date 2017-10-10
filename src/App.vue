5b9208b6626d8b8c578ce8991f1b5c6cde3c7602
m978718484 <m978718484@163.com>
2017-10-10 19:14:09 +0800

    t

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div>
     <img src="./assets/filter_bg.jpg" style="width: 100%">
+    <swiper :list="demo03_list" auto style="width:80%;margin:0 auto;" height="180px" dots-class="custom-bottom" dots-position="center"></swiper>
     <search
     @result-click="resultClick"
     @on-change="getResult"
@@ -13,12 +14,26 @@
     @on-cancel="onCancel"
     @on-submit="onSubmit"
     ref="search"></search>
+    <swiper auto height="30px" direction="vertical" :interval=2000 class="text-scroll" :show-dots="false">
+      <swiper-item><p>义务爱了 完成传奇世界H5-王者归来任务 获得20金币</p></swiper-item>
+      <swiper-item><p>基本世神 兑换《传奇世界H5》畅玩级礼包 消耗30金币</p></swiper-item>
+      <swiper-item><p>零哥章魚 完成传奇世界H5-王者归来任务 获得30金币</p></swiper-item>
+      <swiper-item><p>做迎而為 兑换【饿了么】畅享美食红包 消耗20金币</p></swiper-item>
+      <swiper-item><p>只知道不知道 兑换【饿了么】畅享美食红包 消耗20金币</p></swiper-item>
+      <swiper-item><p>基本世神 兑换《传奇世界H5》畅玩级礼包 消耗30金币</p></swiper-item>
+    </swiper>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { Search, Group, Cell, XButton } from 'vux'
+import { Swiper, SwiperItem, Search, Group, Cell, XButton } from 'vux'
+const imgList = [
+  './assets/banner.jpg',
+  './assets/banner-1.jpg',
+  './assets/banner-2.jpg',
+  './assets/banner-3.jpg'
+]
 export default {
   name: 'app',
   components: {
@@ -26,6 +41,11 @@ export default {
     Group,
     Cell,
     XButton
+  },
+  data () {
+    return {
+      demo03_list: demoList
+    }
   }
 }
 </script>
