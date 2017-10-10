6a3dfc54c36732e9ecf379771e692ee594f1465f
m978718484 <m978718484@163.com>
2017-10-10 17:32:00 +0800

    t

--- src/components/Vux.vue
+++ src/components/Vux.vue
@@ -2,15 +2,27 @@
   <div>
     <group-title>3 columns</group-title>
     <grid>
-      <grid-item link="/component/cell" label="跳转到主页">
+      <grid-item link="/component/cell" label="公告">
         <img slot="icon" src="../assets/home.png">
       </grid-item>
-      <grid-item :link="{ path: '/component/cell'}" label="跳转到主页">
+      <grid-item :link="{ path: '/component/cell'}" label="转让项目">
         <img slot="icon" src="../assets/home.png">
       </grid-item>
       <grid-item link="/component/cell" @on-item-click="onItemClick">
         <img slot="icon" src="../assets/home.png">
-        <span slot="label">跳转到主页</span>
+        <span slot="label">转让结果</span>
+      </grid-item>
+    </grid>
+    <grid>
+      <grid-item link="/component/cell" label="招标项目">
+        <img slot="icon" src="../assets/home.png">
+      </grid-item>
+      <grid-item :link="{ path: '/component/cell'}" label="中标结果">
+        <img slot="icon" src="../assets/home.png">
+      </grid-item>
+      <grid-item link="/component/cell" @on-item-click="onItemClick">
+        <img slot="icon" src="../assets/home.png">
+        <span slot="label">关于我们</span>
       </grid-item>
     </grid>
   </div>
